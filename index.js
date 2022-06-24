30233794d88032b82bfa7787a2f80dd17eb7f65f
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-06-24 09:05:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  // reverse the input string
+  const reverseWord = reverse(word);
+  // compare the reversed string to the input
+  return word === reverseWord;
 }
 
 /* 
   Add your pseudocode here
+  
+  reverse the input string
+  if the reversed string is the same as the input
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  create an array from the input string
+  reverse the array
+  create a string from the reversed array
+  return the reversed string
 */
 
 // You can run `node index.js` to view these console logs
